ab4e19014c64d1b48f945e3e5976718bce3223ee
rrliang <42505429+rrliang@users.noreply.github.com>
2021-12-12 01:42:35 -0500

    Fixed issue of clicking too fast through slideshow

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ window.onload = () => {
     let index = 0;
 
     let checkSlide = () => {
-        console.log(index);
         if (index > 0 && index < 4) {
             LEFTBUTTON.classList.remove(`hidden`);
             RIGHTBUTTON.classList.remove(`hidden`);
@@ -18,21 +17,30 @@ window.onload = () => {
         }
     };
 
+
     let moveImages = (pixelsMoved) => {
         let currentLeft = parseInt((window.getComputedStyle(PARENT)).getPropertyValue(`left`));
-        MOVE.style.left = currentLeft + pixelsMoved + "px";
-        PARENT.classList.add(`move`);
+        console.log(currentLeft);
+        if (currentLeft === 510 || currentLeft === -70 || currentLeft === -650
+            || currentLeft === -1230 || currentLeft === -1810 || currentLeft === -1230) {
+            console.log(currentLeft);
+            MOVE.style.left = currentLeft + pixelsMoved + "px";
+            PARENT.classList.add(`move`);
+            if (pixelsMoved < 0) {
+                index += 1;
+            } else {
+                index -= 1;
+            }
+        }
     };
 
     RIGHTBUTTON.addEventListener(`click`, () => {
         moveImages(-WIDTH);
-        index += 1;
         checkSlide();
     });
 
     LEFTBUTTON.addEventListener(`click`, () => {
         moveImages(WIDTH);
-        index -= 1;
         checkSlide();
     });
 };
